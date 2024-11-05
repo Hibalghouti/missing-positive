9cf671127a3970bb7d1644d57a8ecf9f4a65dbb5
Hiba Al Ghouti <114570480+Hibalghouti@users.noreply.github.com>
2024-11-05 17:46:32 +0200

    Update missingpositive.py

--- missingpositive.py
+++ missingpositive.py
@@ -1,11 +1,10 @@
 def first_missing_positive(numbers):
-    #removes duplicate when converting list to a set
-    num_set = set(numbers) 
 
     # Start checking from the first positive integer
     for i in range(1, len(numbers) + 1): 
-        if i not in num_set:
+        if i not in numbers:
             return i
 
-print(first_missing_positive([3, 4, -1, 1]))
+print(first_missing_positive( [1, 2, 0]))
+print(first_missing_positive(  [3, 4, -1, 1]))  
 
